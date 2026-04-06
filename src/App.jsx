17929d17535ceddd151a16a8ff5ffd0235db79ac
Fekry <mahmoud.qasim@flairstech.com>
2026-04-06 19:08:00 +0200

    feat: US-001-US-005 - Add React Router setup with Navbar and page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import HelloWorld from './pages/HelloWorld'
 
+const containerStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100vh'
+}
+
+const contentStyle = {
+  flex: 1,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+}
+
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/hello" element={<HelloWorld />} />
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <div style={containerStyle}>
+        <Navbar />
+        <main style={contentStyle}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/hello" element={<HelloWorld />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   )
 }
 
