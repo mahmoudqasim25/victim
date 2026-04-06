cf20f6083e1ba78362865197756d33694a1328a8
Fekry <mahmoud.qasim@flairstech.com>
2026-04-06 17:14:30 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        fontSize: '2rem',
+        textAlign: 'center'
+      }}
+    >
+      Hello, World!
+    </div>
+  )
+}
+
+export default App
