7d2534e9cf29bf7675c94c5aebf24c1be99f89e7
Fekry <mahmoud.qasim@flairstech.com>
2026-04-23 18:43:27 +0200

    feat: [US-001] - Create Demo.jsx page component

---
+++ src/pages/Demo.jsx
@@ -0,0 +1,9 @@
+function Demo() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h1>welcome from demo 1</h1>
+    </div>
+  );
+}
+
+export default Demo;
